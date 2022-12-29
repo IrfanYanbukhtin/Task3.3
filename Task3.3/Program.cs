d88432956c4e3c73b69977c18a5e63068adfbfd2
Irfan Yanbukhtin <woolfyko@gmail.com>
2022-12-29 19:29:41 +0400

    task3.3

---
+++ Task3.3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal n = decimal.Parse(Console.ReadLine());
+            int real = (int)n;
+            decimal kesr = n - real;
+            decimal x = (real / 1000M);
+            decimal reverse = x + kesr * 1000;
+            Console.WriteLine($"{reverse}");
+        }
+    }
+}
